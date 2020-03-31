ab0bfb781a6ddee4ae49c62200ff60b344dc4072
mcanute <mcanute@sfu.ca>
2020-03-31 12:36:46 -0700

    modifications to second-pass api call

--- DataCollection/SecondPass.py
+++ DataCollection/SecondPass.py
@@ -8,20 +8,22 @@ access_key = '188667470-hunlrSR3Dd4zpap331fT4a56w0TEl1JMMllFdpTT'
 access_secret = 'YK44l9JNDAgZwyFZUiqYMlHD4ZpLX0v07EujRjx6XGsoa'
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True)
 
 COLUMNNAMES = ['ScreenName','Type','TweetID','TweetText','TweetDateTime'
               ,'Followers','Friends','Statuses','Favourites']
 
+tweets_df = pd.read_csv('../data/sample.csv',header=None)
+tweets_df.columns = ['Index','TweetID','UserName','TweetText','TweetDateTime','Followers','UserID']
+tweets_df = tweets_df[['TweetID','UserName','TweetText','TweetDateTime','Followers','UserID']]
+tweets_df['TweetID'] = tweets_df['TweetID'].astype(str)
 
-tweets_df = pd.read_csv('Saved.csv')
 tweetIDs = tweets_df['TweetID'].values
-
 
 Table = []
 Row = []
 tweetsbuffer = []
-for tweetid in responseTweetsArray:
+for tweetid in tweetIDs:
     if len(tweetsbuffer) < 100:
         tweetsbuffer.append(tweetid)
     else:
@@ -53,4 +55,22 @@ if len(tweetsbuffer) > 0:
         Table.append(Row)
         Row = []
 
-# need a try catch for when it doesn't exist.
+tweets_still_alive_df = pd.DataFrame(Table)
+tweets_still_alive_df.columns = ['TweetID','TweetDateTime','TweetText','ScreenName','Followers','Following','TweetsCount','LikesCount']
+tweets_still_alive_df['TweetID'] = tweets_still_alive_df['TweetID'].astype(str)
+
+tweets_df_merged_still_alive_df = pd.merge(tweets_df,tweets_still_alive_df,how='left',on='TweetID')
+tweets_df_merged_still_alive_df[tweets_df_merged_still_alive_df['TweetText_y'] != tweets_df_merged_still_alive_df['TweetText_y']]
+
+def was_deleted(tweet_text):
+    if tweet_text != tweet_text:
+        return 1
+    else:
+        return 0
+
+tweets_df_merged_still_alive_df['WasDeleted'] = tweets_df_merged_still_alive_df['TweetText_y'].apply(was_deleted)
+
+tweets_new_df = tweets_df_merged_still_alive_df[['TweetID','UserName','TweetText_x','TweetDateTime_x','Followers_x','UserID','WasDeleted']]
+tweets_new_df.columns = ['TweetID','UserName','TweetText','TweetDateTime','Followers','UserID','WasDeleted']
+
+tweets_new_df.to_csv('../data/sample_with_label.csv')
